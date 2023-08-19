1c65910f45f4b4d103bac407e5ba796f13cce498
Iru Sensei <iru@despera.space>
2023-08-19 17:24:17 +0200

    kobold and llama

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 podman build -t repo.despera.space/iru/rocm:5.4.3 -f Dockerfile.5.4.3 .
 podman build -t repo.despera.space/iru/rocm:5.4.3-torch5.4.2 -f torch/Dockerfile.5.4.3 .
+podman build -t repo.despera.space/iru/rocm:5.4.3-koboldcpp . -f kobold/Dockerfile.5.4.3
+podman build -t repo.despera.space/iru/rocm:5.4.3-llamacpp . -f llamacpp/Dockerfile.5.4.3
 podman build -t repo.despera.space/iru/rocm:5.2.3 -f Dockerfile.5.2.3 . 
 podman build -t repo.despera.space/iru/rocm:5.2.3-torch-1.13.1 -f torch/Dockerfile.5.2.3 .
---
+++ push_all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+podman push repo.despera.space/iru/rocm:5.4.3
+podman push repo.despera.space/iru/rocm:5.4.3-torch5.4.2
+podman push repo.despera.space/iru/rocm:5.4.3-koboldcpp
+podman push repo.despera.space/iru/rocm:5.4.3-llamacpp
+podman push repo.despera.space/iru/rocm:5.2.3
+podman push repo.despera.space/iru/rocm:5.2.3-torch-1.13.1
